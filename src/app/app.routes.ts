668aed4a7a9169cfcf9ecf3af3c78311577da50f
Ravali960388 <159105852+Ravali960388@users.noreply.github.com>
2025-10-16 14:44:43 +0530

    commited

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { DemoModalboxComponent } from './demo-modalbox/demo-modalbox.component';
 
 export const routes: Routes = [
-    {path:'', component:HomeComponent}
+    {path:'', component:HomeComponent},
+    {path:'demo_modalbox', component:DemoModalboxComponent}
 ];
