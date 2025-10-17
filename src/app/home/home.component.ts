8a8af6f6f11de3f7435140fcb11873b77a5434e3
Ravali960388 <159105852+Ravali960388@users.noreply.github.com>
2025-10-17 11:44:41 +0530

    changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, RouterOutlet, FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+getstarteddemo() {
+throw new Error('Method not implemented.');
+}
+  demoForm!: FormGroup;
+  
 
+  constructor(private fb: FormBuilder, private router:Router) {}
+
+  ngOnInit(): void {
+    this.demoForm = this.fb.group({
+      name: ['', Validators.required],
+      phone: [''],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+    });
+
+    // const modalElement = document.getElementById('demoModal');
+    // if (modalElement) {
+    //   this.demoModal = new bootstrap.Modal(modalElement);
+    // }
+  }
+
+  // getstarteddemo() {
+  //   if (this.demoModal) {
+  //     this.demoModal.show();
+  //   }
+  // }
+
+  // submitDemoForm() {
+  //   if (this.demoForm.valid) {
+  //     console.log('Form Data:', this.demoForm.value);
+  //     alert('Your demo request has been submitted successfully!');
+  //     this.demoForm.reset();
+  //     this.demoModal.hide();
+  //   } else {
+  //     alert('Please fill in all required fields!');
+  //   }
+  // }
 }
