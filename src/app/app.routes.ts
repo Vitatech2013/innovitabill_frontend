5e60b76a4b6dd4ea48ae56d246cd98d584cc8ab9
Ravali960388 <159105852+Ravali960388@users.noreply.github.com>
2025-10-23 15:32:50 +0530

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CashierComponent } from './cashier/cashier.component';
 
 
 export const routes: Routes = [
-    {path:'', component:HomeComponent}
+    {path:'', component:HomeComponent},
+    {path:'cashier', component:CashierComponent}
     
 ];
