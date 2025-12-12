1fd7361473ad22ecca0d25290d9ebeb7f3ceec58
Ravali960388 <159105852+Ravali960388@users.noreply.github.com>
2025-12-12 11:48:40 +0530

    inactive_businesstype, forgot_password changes

--- src/app/Superadmin/inactive-businesstype/inactive-businesstype.component.ts
+++ src/app/Superadmin/inactive-businesstype/inactive-businesstype.component.ts
@@ -2,20 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { BillingService } from '../../Services/billing.service';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 declare var bootstrap: any;
 
 @Component({
   selector: 'app-inactive-businesstype',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule,RouterLink,ReactiveFormsModule,FormsModule],
   templateUrl: './inactive-businesstype.component.html',
   styleUrl: './inactive-businesstype.component.css'
 })
 export class InactiveBusinesstypeComponent  implements OnInit{
 selectedB_type: any;
 inactiveBusiness: any[] = [];
-
+ searchTerm: string = '';
   constructor(private inactiveApi:BillingService) {}
 
   ngOnInit(): void {
@@ -35,5 +36,14 @@ inactiveBusiness: any[] = [];
     const modalEl = document.getElementById('deleteModal');
     const modal = new bootstrap.Modal(modalEl);
     modal.show();
+  }
+  filteredBusiness() {
+    if (!this.searchTerm) return this.inactiveBusiness;
+    const term = this.searchTerm.toLowerCase();
+    return this.inactiveBusiness.filter((b) =>
+      Object.values(b).some((val) =>
+        val?.toString().toLowerCase().includes(term)
+      )
+    );
   }
 }
